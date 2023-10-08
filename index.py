c6143081b546dad2037638e8a9e0bac87924fdc9
moheros <qxin829@gmail.com>
2023-10-08 19:12:20 +0900

    小浜市と浜松市追加

--- index.py
+++ index.py
@@ -9,7 +9,6 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 import pytz
-# import sys
 
 def get_weather_icon(icon_str):
     if icon_str == "01d" or icon_str == "01n":
@@ -41,23 +40,23 @@ def send_to_line(df):
     for k, v in df:
         if count == 1:
             texts.append(f"【{k}】")
+            place = ""
+
             for _, d in v.iterrows():
+                if place == "" or place != f"{d['place']}":
+                    texts.append(f"{d['place']}")
                 texts.append(
                     f"{d['time']}時 {get_weather_icon(d['icon'])} {d['temp']}℃　 [{d['rain']}mm/3h]"
                 )
+                place = f"{d['place']}"
+
             texts.append("")
             count += 1
 
-    # line_bot = LineBotApi(os.getenv("LINE_ACCESS_TOKEN"))
-    # line_bot = LineBotApi(os.environ["LINE_ACCESS_TOKEN"])
     line_bot = LineBotApi(os.environ.get("LINE_ACCESS_TOKEN"))
-    # line_bot = LineBotApi(sys.argv[3])
         
     try:
-        # line_bot.multicast(os.getenv("LINE_USER_ID").split(","), TextSendMessage(text="\n".join(texts)))
-        # line_bot.multicast(os.environ["LINE_USER_ID"].split(","), TextSendMessage(text="\n".join(texts)))
         line_bot.multicast(os.environ.get("LINE_USER_ID").split(","), TextSendMessage(text="\n".join(texts)))
-        # line_bot.multicast(sys.argv[4].split(","), TextSendMessage(text="\n".join(texts)))
         print('成功')
     except LineBotApiError as e:
         print('send_to_line関数内でエラーが発生しました。')
@@ -67,31 +66,35 @@ def send_to_line(df):
 def main():
     load_dotenv()  # .env ファイルから環境変数を読み込む
     url = "http://api.openweathermap.org/data/2.5/forecast"
-    # id = os.getenv("OWM_PLACE_ID")
-    # id = os.environ["OWM_PLACE_ID"]
     id = os.environ.get("OWM_PLACE_ID")
-    # id = sys.argv[1]
-    # api_key = os.getenv("OWM_API_KEY")
-    # api_key = os.environ["OWM_API_KEY"]
     api_key = os.environ.get("OWM_API_KEY")
-    # api_key = sys.argv[2]
-
-    res = urlopen(f"{url}?id={id}&appid={api_key}&lang=ja&units=metric").read()
-    res_json = json.loads(res)
 
     arr_rj = []
-    for rj in res_json["list"]:
-        conv_rj = {}
-        timezone = pytz.timezone("Asia/Tokyo")
-        timestamp = datetime.fromtimestamp(rj["dt"], tz=timezone)
-        weekday_japanese = ["月", "火", "水", "木", "金", "土", "日"][timestamp.weekday()]
-        conv_rj["date"] = timestamp.strftime("%m月%d日 {}曜日".format(weekday_japanese))
-        conv_rj["time"] = timestamp.strftime("%H")
-        conv_rj["description"] = rj["weather"][0]["description"]
-        conv_rj["icon"] = rj["weather"][0]["icon"]
-        conv_rj["temp"] = round(rj["main"]["temp"])
-        conv_rj["rain"] = round(rj["rain"]["3h"], 1) if "rain" in rj else 0
-        arr_rj.append(conv_rj)
+
+    for count, id in enumerate(ids, start=1):
+        res = urlopen(f"{url}?id={id}&appid={api_key}&lang=ja&units=metric").read()
+        res_json = json.loads(res)
+
+        if count == 1:
+            owm_place = "■大阪市の天気"
+        elif count == 2:
+            owm_place = "■小浜市の天気"
+        elif count == 3:
+            owm_place = "■浜松市の天気"
+
+        for rj in res_json["list"]:
+            conv_rj = {}
+            timezone = pytz.timezone("Asia/Tokyo")
+            timestamp = datetime.fromtimestamp(rj["dt"], tz=timezone)
+            weekday_japanese = ["月", "火", "水", "木", "金", "土", "日"][timestamp.weekday()]
+            conv_rj["date"] = timestamp.strftime("%m月%d日 {}曜日".format(weekday_japanese))
+            conv_rj["place"] = owm_place
+            conv_rj["time"] = timestamp.strftime("%H")
+            conv_rj["description"] = rj["weather"][0]["description"]
+            conv_rj["icon"] = rj["weather"][0]["icon"]
+            conv_rj["temp"] = round(rj["main"]["temp"])
+            conv_rj["rain"] = round(rj["rain"]["3h"], 1) if "rain" in rj else 0
+            arr_rj.append(conv_rj)
 
     try:
         send_to_line(pd.DataFrame(arr_rj).groupby("date"))
@@ -100,6 +103,4 @@ def main():
         print('main関数内でエラーが発生しました。')
         print('Error occurred: {}'.format(e))
 
-
-# load_dotenv()
 main()
