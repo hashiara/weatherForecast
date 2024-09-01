1c371e5969decc79e89b3f2e10da33330e38dceb
moheros <qxin829@gmail.com>
2024-09-01 12:43:24 +0900

    路線情報通知来ない原因調査

--- trainStatus.py
+++ trainStatus.py
@@ -80,7 +80,12 @@ def main():
                         # 現在時刻が0時を過ぎていない
                         # rail['status']がlast_train_statusと異なる
                         # last_train_statusがNone
+                        print(type(rail['status']))
+                        print(rail['status'])
+                        print(type(last_train_status))
+                        print(last_train_status)
                         if start_time <= current_time <= end_time and rail['status'] != last_train_status:
+                            print("いふ")
                             send_to_line(line_access_token, user_id, rail, formatted_last_updated, src)
                             try:
                                 cursor.execute("""UPDATE train 
@@ -91,6 +96,7 @@ def main():
                             except Exception as e:
                                 print(f"Error updating {user_id}: {e}")
                         elif end_time < current_time:
+                            print("えるいふ")
                             try:
                                 cursor.execute("""UPDATE train 
                                                SET last_train_status = Null
@@ -100,6 +106,7 @@ def main():
                             except Exception as e:
                                 print(f"Error updating {user_id}: {e}")
                         else:
+                            print("えるす")
                             continue
 
     # 接続を閉じる
